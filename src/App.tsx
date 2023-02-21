c2aef507a89fc39729fde8d0409a308eb4925428
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2023-02-21 16:54:15 -0300

    Erase solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,26 @@
 import { useEffect, useRef, useState } from 'react';
 
-import usePosts from './hooks/usePosts';
+/*
+TODO:
+- [ ] Load posts from API and show them in a list
+- [ ] Show loading indicator when loading posts
+- [ ] Show error message when loading posts fails
+- [ ] Show user names instead of user IDs for each post
+- [ ] Show "No posts" when there are no posts
+- [ ] Add limit and show "Load more" button when there are more posts to load
+- [ ] Add infinite scrolling
+*/
+
+/*
+Endpoints:
+Posts: `${import.meta.env.VITE_API_BASE_URL}/posts?limit=${limit}&skip=${skipAmount}`
+User: `${import.meta.env.VITE_API_BASE_URL}/users/${userId}`
+*/
 
 function App() {
-  const [skip, setSkip] = useState(0);
-  const { posts, error: postsErrors, loading: loadingPosts } = usePosts(skip, 10);
-
-  const innerDivRef = useRef<HTMLDivElement>(null);
-  const [scrollDivRef, setScrollDivRef] = useState<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const buffer = 50;
-    if (scrollDivRef === null) {
-      return;
-    }
-
-    const onScroll = () => {
-      if (innerDivRef.current === undefined || innerDivRef.current === null) {
-        return;
-      }
-
-      const { bottom } = scrollDivRef.getBoundingClientRect();
-      const { bottom: innerDivBottom } = innerDivRef.current.getBoundingClientRect();
-
-      if (!loadingPosts && bottom >= innerDivBottom - buffer) {
-        setSkip((skip) => skip + 10);
-      }
-    };
-
-    scrollDivRef.addEventListener('scroll', onScroll);
-
-    return () => scrollDivRef.removeEventListener('scroll', onScroll);
-  }, [scrollDivRef]);
-
   return (
     <main className="flex flex-col h-screen w-screen p-5">
       <h1 className="font-bold text-3xl">Welcome!</h1>
-
-      {loadingPosts && <span>Loading...</span>}
-
-      <div ref={(div) => setScrollDivRef(div)} className="overflow-auto">
-        <div ref={innerDivRef}>
-          {posts.map((post) => (
-            <div className="p-2 shadow-sm" key={post.id}>
-              <h2 className="text-xl font-bold">{post.title}</h2>
-              <p className="truncate">{post.body}</p>
-              <div className="flex justify-between items-center text-center">
-                <div className="flex gap-2">
-                  {post.tags.map((tag) => (
-                    <h3 className="bg-orange-500 text-white p-1 rounded">{tag}</h3>
-                  ))}
-                </div>
-
-                <h4 className="font-bold">
-                  {post.user.firstName} {post.user.lastName}
-                </h4>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <span>{JSON.stringify(postsErrors)}</span>
     </main>
   );
 }
